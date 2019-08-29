6b43eb8563fdd1d309ebbc76925c8fb553f66acc
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-08-29 18:09:30 -0500

    Created File Demo
    In Class Demo for files

---
+++ src/assignment1/FileDemo.java
@@ -0,0 +1,35 @@
+package assignment1;
+import java.util.*;
+import java.io.*;
+
+public class FileDemo
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner inputStream = null;
+        PrintWriter outputStream = null;
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("Input file: ");
+		String inputFileName = console.next();
+		System.out.print("Output file: ");
+      	String outputFileName = console.next();
+
+        inputStream = new Scanner(new FileInputStream(inputFileName));
+           outputStream = new PrintWriter(new FileOutputStream(outputFileName));
+
+        String line = null;
+        int count = 0;
+
+        while (inputStream.hasNextLine( ))
+        {
+            line = inputStream.nextLine( );
+            count++;
+            outputStream.println(count + ":" + line);
+        }
+
+        inputStream.close( );
+        outputStream.close( );
+    }
+
+}
