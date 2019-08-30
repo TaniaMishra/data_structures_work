3b03696826513a2aa455e4a2893bcc59b52e3beb
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-08-30 13:32:55 -0500

    Working on EmailMerge Class

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -9,14 +9,24 @@ public class EmailMerge {
 		Scanner inputPeople = new Scanner(new FileInputStream("people.txt"));
 		Scanner inputTemp = new Scanner(new FileInputStream("template.txt"));
 		
-		while (inputPeople.hasNextLine( ))
+		while (inputPeople.hasNextLine())
         {
-            System.out.println(inputPeople.nextLine( ));
+            String line = inputPeople.nextLine();
+            int space1 = line.indexOf(" ");
+            int space2 = line.indexOf(" ", space1+1);
+            String name = line.substring(0, space1);
+            String age = line.substring(space1+1, space2);
+            String gender = line.substring(space2+1);
+            File fileObject = new File(name);
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(name));
+            //String emailContents =
+            // outputStream.println();
+
+
            
         }
 		
 		//String fileName;
-        //PrintWriter outputStream = new PrintWriter(new FileOutputStream(fileName));
 
 		
 	}
