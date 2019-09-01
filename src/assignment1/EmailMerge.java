8d56762f19189dda00d5c8eb67079986587808ba
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-09-01 10:12:03 -0500

    Working on EmailMerge

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -8,7 +8,7 @@ public class EmailMerge {
 	public static void main(String[] args) throws IOException {
 		Scanner inputPeople = new Scanner(new FileInputStream("people.txt"));
 		Scanner inputTemp = new Scanner(new FileInputStream("template.txt"));
-		
+
 		while (inputPeople.hasNextLine())
         {
             String line = inputPeople.nextLine();
@@ -19,8 +19,30 @@ public class EmailMerge {
             String gender = line.substring(space2+1);
             File fileObject = new File(name);
             PrintWriter outputStream = new PrintWriter(new FileOutputStream(name));
-            //String emailContents =
-            // outputStream.println();
+            String emailContents = "";
+            int startIndex = 0;
+            while (inputTemp.hasNextLine()) {
+            	String emailLine = inputTemp.nextLine();
+            	if (emailLine.contains("<<N>>")) {
+            		int nameHolder = emailLine.indexOf("<<N>>");
+            		emailContents.concat(emailLine.substring(startIndex, nameHolder));
+            		emailContents.concat(name);
+            		startIndex = nameHolder + 5;
+            	}
+            	else if (emailLine.contains("<<A>>")) {
+            		int ageHolder = emailLine.indexOf("<<A>>");
+            		emailContents.concat(emailLine.substring(startIndex, ageHolder));
+            		emailContents.concat(age);
+            		startIndex = ageHolder + 5;
+            	}
+            	else if (emailLine.contains("<<G>>")) {
+            		int genderHolder = emailLine.indexOf("<<G>>");
+            		emailContents.concat(emailLine.substring(startIndex, genderHolder));
+            		startIndex = genderHolder + 5;
+            	}
+            }
+           // emailContents.concat()
+            // outputStream.println(emailContents);
 
 
            
