b8179f3f931e4a81e161d81afc73a5f8888a196d
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-09-16 18:51:28 -0500

    All Classes & Interfaces for Array Bounded Stack

---
+++ src/assignment3/StackUnderflowException.java
@@ -0,0 +1,11 @@
+package assignment3;
+
+public class StackUnderflowException extends RuntimeException {
+	public StackUnderflowException() {
+		super();
+	}
+
+	public StackUnderflowException(String message) {
+		super(message);
+	}
+}
