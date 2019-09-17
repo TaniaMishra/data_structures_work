6fb6396b1962c21fea59a9e885a7be556c0c5168
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-09-16 19:17:26 -0500

    Began Test Class

---
+++ src/assignment3/ArrayBoundedStackTestClass.java
@@ -0,0 +1,25 @@
+package assignment3;
+
+import java.util.*;
+
+public class ArrayBoundedStackTestClass {
+	public static void main(String[] args) {
+		ArrayBoundedStack stack = new ArrayBoundedStack(20);
+		Random rand = new Random();
+		
+		//fill stack with random values
+		for (int i = 0; i < 20; i++) {
+			stack.push(rand.nextInt());
+		}
+		//test remove method with 4 values, 12 values, 6 values (should throw exception)
+		System.out.println(stack.topIndex);
+		stack.remove(4);
+		System.out.println(stack.topIndex);
+		stack.remove(12);
+		System.out.println(stack.topIndex);
+		stack.remove(6);
+		System.out.println(stack.topIndex);
+		
+	}
+
+}
