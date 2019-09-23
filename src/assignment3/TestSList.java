2a15802e029badeed6fcfd8a324950a20e0fbf03
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-09-23 12:00:48 -0500

    Started In Class Exercise (Linked Lists)

---
+++ src/assignment3/TestSList.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+public class TestSList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SList<String> myList = new SList<String>();
+		myList.add("A");
+		myList.add("B");
+		myList.add("C");
+		System.out.println(myList);
+
+	}
+
+}
