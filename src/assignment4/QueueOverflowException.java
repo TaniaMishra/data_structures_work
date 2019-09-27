fc631594fa503d945114b25424a4986981953c94
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-09-27 11:33:41 -0500

    Queue Files
    Inteface, exceptions, linked lists

---
+++ src/assignment4/QueueOverflowException.java
@@ -0,0 +1,14 @@
+package assignment4;
+
+public class QueueOverflowException extends RuntimeException
+{
+  public QueueOverflowException()
+  {
+    super();
+  }
+
+  public QueueOverflowException(String message)
+  {
+    super(message);
+  }
+}
