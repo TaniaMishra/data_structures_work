98540b527e5976504b2c18b2bacad3ae91eb9af5
Tania Mishra <22mishrt@elmbrookstudents.org>
2019-11-03 11:03:19 -0600

    Working on Assignment 5

--- src/assignment5/AccountHashMap.java
+++ src/assignment5/AccountHashMap.java
@@ -8,57 +8,58 @@ import java.util.*;
 
 public class AccountHashMap {
 	public static void main(String[] args) {
-		// Create a HashMap to store Account objects.
+		// HashMap --> stores BankAccount objects.
 		Map<String, BankAccount> accountMap = new HashMap<String, BankAccount>();
 
-		// Create some account objects
-		BankAccount johnAccount = new BankAccount(1000);
-		BankAccount sallyAccount = new BankAccount(2000);
-		BankAccount peterAccount = new BankAccount(3000);
-		BankAccount testAccount = new BankAccount(10000);
-
-		// Put some mappings into the HashMap. In each mapping,
-		// account number is the key and bankaccount object is the value
-		accountMap.put("101", johnAccount);
-		accountMap.put("102", sallyAccount);
-		accountMap.put("103", peterAccount);
-		if (!accountMap.containsKey("103"))
-			accountMap.put("103", testAccount);
-
-		// Search for a sample account number
-		System.out
-				.println("\nSearching for the account with account number 103");
-		BankAccount foundAccount = accountMap.get("103");
-
-		// If the account was found, display the account balance.
-		if (foundAccount != null)
-			System.out.println(foundAccount.getBalance());
-		else
-			System.out.println("Account not found in the system");
-
-		// Get a set containing the keys in this map.
-		Set<String> keys = accountMap.keySet();
-
-		// Iterate through the keys, printing each one.
-		System.out.println("Here are the keys and value pairs:");
-		for (String k : keys) {
-			System.out.println(k + ":" + accountMap.get(k));
-		}
-
-		// Get a collection containing the values.
-		Collection<BankAccount> values = accountMap.values();
-
-		// Iterate through the values, printing each one.
-		System.out.println("\nHere are the values:");
-		for (BankAccount account : values)
-			System.out.println(account.getBalance());
+		// Put mappings into the HashMap --> key = string, account number. value = BankAccount
+		accountMap.put("101", new BankAccount(1000));
+		accountMap.put("102", new BankAccount(1000));
+		accountMap.put("103", new BankAccount(1000));
+		
+		//Call method to display the accounts with equal balances
+		displayAccountsWithEqualBalances(accountMap);
 	}
 	
-	public void displayAccountsWithEqualBalances(Map accountMap) {
+	public static void displayAccountsWithEqualBalances(Map<String, BankAccount> accountMap) {
 		Set<String> keys = accountMap.keySet();
-		ArrayList<BankAccount> myValues = new ArrayList<BankAccount>();
-		for (String k : keys) {
-		//	myValues.add(accountMap.get(k));
+		Map<String, Double> valueMap = new HashMap<String, Double>();
+		for (Map.Entry<String, BankAccount> x : accountMap.entrySet()) {
+			valueMap.put(x.getKey(), x.getValue().getBalance());
+		}
+		ArrayList<Double> balances = new ArrayList<Double>();
+		for (Map.Entry<String, Double> x : valueMap.entrySet()) {
+			balances.add(x.getValue());
+		}
+		boolean duplicate = false;
+		double tester = balances.remove(0);
+		while (balances.size() > 0) {
+			if (balances.contains(tester)) {
+				duplicate = true;
+			}
+			if (duplicate == true) {
+				ArrayList<String> accountNumbers = new ArrayList<String>();
+				for (Map.Entry<String, Double> x : valueMap.entrySet()) {
+					if (x.getValue() == tester) {
+						accountNumbers.add(x.getKey());
+					}
+				}
+				System.out.print("Account numbers ");
+				for (int i = 0; i < accountNumbers.size(); i++) {
+					if (i == accountNumbers.size()-1) {
+						System.out.print(accountNumbers.get(i) + " ");
+					}
+					else {
+						System.out.print(accountNumbers.get(i) + ", ");
+					}
+				}
+				System.out.print("with equal balance of " + tester + "\n");
+				for (int i = balances.size()-1; i >= 0; i--) {
+					if (balances.get(i) == tester) {
+						balances.remove(i);
+					}
+				}
+			}
+			tester = balances.remove(0);
 		}
 	}
 }
